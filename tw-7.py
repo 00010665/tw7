17653b588a80108cbb0f065a34d94417116f337c
00010665 <72818278+00010665@users.noreply.github.com>
2020-11-11 10:36:23 +0500

    Task 10

---
+++ tw-7.py
@@ -0,0 +1,5 @@
+myFinalMarks = {'CSF': 75, 'FunPro': 80, 'WT': 85}
+how_many_modules = len(myFinalMarks)
+all_marks_sum = sum(myFinalMarks.values())
+Average_mark = all_marks_sum / how_many_modules
+print('It is your average mark', Average_mark)
